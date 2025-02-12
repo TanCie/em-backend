768a7e5326077961d32c4a6a7124ba92b621bf50
Tanushree Chakravorty <tanushree998@gmail.com>
2025-02-12 20:23:54 +0530

    eventr done

--- src/server.js
+++ src/server.js
@@ -14,7 +14,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173", ""],
+    credentials: true,
+}));
 app.use(express.json({ limit: "10mb", extended: true }));
 
 app.use("/api/events", eventRoutes);
